15aa03761aad4068fb226e9cf6ea8ce281b80109
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2019-11-07 18:12:06 +0900

    Test secrets

--- src/index.js
+++ src/index.js
@@ -71,5 +71,6 @@ if (require.main === module) {
     testPath: process.argv[2],
     reportFilePath: config.reportFilePath
   };
+  console.log(`secret name = ${process.env.secret_name}`);
   main(options);
 }
